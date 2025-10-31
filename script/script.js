fe6b479e88b6d906235a452245a9c55cced45af0
Alif1409 <fathaaliftha@gmail.com>
2025-10-31 22:31:17 +0700

    10/31/2025 10.31

---
+++ script/script.js
@@ -0,0 +1,57 @@
+let todos = [];
+
+function validateForm(todo, date) {
+    if (todo.trim() === '' || date === '') {
+        alert('Please fill in all fields.');
+        return false;
+    }
+    return true;
+}
+
+/// Function to add a new todo item
+function addTodo() {
+    const todoInput = document.getElementById('todo-input').value;
+    const todoDate = document.getElementById('due-date').value;
+
+    if (!validateForm(todoInput, todoDate)) {
+        alert('Please fill in all fields.');
+    } else {
+
+        console.log('Schedule Added:', todoInput, 'Due date:', todoDate);
+  
+        todos.push({ task: todoInput, dueDate: todoDate });
+
+        renderTodos();
+        
+        console.log('Current Schedule: ', todos);
+        alert('Schedule added successfully!');
+
+        // Clear input fields after adding
+        document.getElementById('todo-input').value = '';
+        document.getElementById('due-date').value = '';
+    }
+}
+
+/// Placeholder function for future feature
+function deleteTodo() {
+
+}
+
+/// Placeholder functions for future features
+function filterTodo() {
+
+}
+
+
+function renderTodos() {
+
+    const todoList = document.getElementById('todo-list');
+    todoList.innerHTML = '';
+
+    todos.forEach((todo, index) => {
+        todoList.innerHTML += `<li>
+            <span>${todo.task} - Due: ${todo.dueDate}</span>
+            <button onclick="deleteTodo(${index})">Delete</button>
+        </li>`;
+    });
+}
